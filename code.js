064a873b265c9abbfd8221ce5a212b1f7c835111
Sai Dithvika <saidithvika@gmail.com>
2024-06-09 22:54:24 +0530

    update html

--- code.js
+++ code.js
@@ -1,15 +1,13 @@
 document.getElementById("form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    let user = document.getElementById("loginUser").value;
-    let pass = document.getElementById("loginPassword").value;
-    if(pass==localStorage.getItem(user)){
-    alert("Hello, " + user + "!");
-    console.log(localStorage.getItem(user))
-      window.location.href="index.html"
-} else{
-    alert("Wrong password or user not found")
-    console.log(localStorage.getItem(user),pass)
-}
-
-  });
-  
+  event.preventDefault();
+  let user = document.getElementById("loginUser").value;
+  let pass = document.getElementById("loginPassword").value;
+  if (localStorage.getItem(user) === pass) {
+      alert("Hello, " + user + "!");
+      console.log(localStorage.getItem(user));
+      window.location.href = "index.html";
+  } else {
+      alert("Wrong password or user not found");
+      console.log(localStorage.getItem(user), pass);
+  }
+});
